1b21e09419c0b5f16a5665cc45ec89b378f99089
Jack Sari <janasarii@gmail.com>
2023-12-17 04:59:44 -0500

    ad

---
+++ src/migrations/1702807174270_sp_get_sizes.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+export class sp_get_sizes1702807174270 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // Agrega aquí tus operaciones de migración
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        // Agrega aquí las operaciones para revertir la migración
+    }
+}
